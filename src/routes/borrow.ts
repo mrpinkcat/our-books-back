df75641e9e4e1188c408570df3020500ad7bf0e9
mrpinkcat <gatien.anizan@gmail.com>
2019-11-06 08:41:10 +0100

    borrow refacto

--- src/routes/borrow.ts
+++ src/routes/borrow.ts
@@ -6,29 +6,43 @@ const User = models.User;
 const Library = models.Library;
 
 export default (req: Request, res: Response) => {
-  if (req.body.isbn && req.body.libraryid && req.body.username) {
-    const isbn = req.body.isbn;
-    const libraryid = req.body.libraryid;
-    const username = req.body.username;
+  console.log('/borrow');
+  if (req.body.id && req.body.libraryId && res.locals.username) {
+    const id = req.body.id;
+    const libraryId = req.body.libraryId;
+    const username = res.locals.username;
     User.findOne({ username })
     .then((userDoc) => {
       if (userDoc) {
-        Library.findOne({ _id: libraryid })
+        Library.findOne({ _id: libraryId })
         .then((LibraryDoc) => {
           if (LibraryDoc) {
-            Book.findOne({ isbn, borrowUsername: undefined })
-            .then((bookDoc) => {
-              if (bookDoc) {
-                console.log('bookDoc');
-                console.log(bookDoc);
-                Book.findByIdAndUpdate(bookDoc.id, { borrowUsername: res.locals.username, libraryId: libraryid })
-                .then((update) => {
-                  console.log('update');
-                  console.log(update);
-                  res.status(200).send({
-                    status: 'Updated',
-                    update,
-                  });
+            // Check si l'utilisateur à pas déjà borrow ce livre
+            Book.find({ borrowUsername: username })
+            .then((checkBookDoc) => {
+              console.log(checkBookDoc);
+              // L'utilisateur n'a pas déjà emprunter de livre
+              if (checkBookDoc.length === 0) {
+                Book.findOne({ _id: id, borrowUsername: undefined })
+                .then((bookDoc) => {
+                  if (bookDoc) {
+                    Book.findByIdAndUpdate(bookDoc.id, { borrowUsername: res.locals.username, libraryId: libraryId })
+                    .then((update) => {
+                      res.status(200).send({
+                        status: 'Updated',
+                        update,
+                      });
+                    })
+                    .catch((err) => {
+                      res.status(500).send({
+                        err,
+                      });
+                    });
+                  } else {
+                    res.status(400).send({
+                      error: `Book '${id}' not found`,
+                    });
+                  }
                 })
                 .catch((err) => {
                   res.status(500).send({
@@ -37,18 +51,13 @@ export default (req: Request, res: Response) => {
                 });
               } else {
                 res.status(400).send({
-                  error: `Book with ISBN '${isbn}' not found`,
+                  error: `User ${username} allready borrow this book (${libraryId})`,
                 });
               }
             })
-            .catch((err) => {
-              res.status(500).send({
-                err,
-              });
-            });
           } else {
             res.status(400).send({
-              error: `Library ${libraryid} not found`,
+              error: `Library ${libraryId} not found`,
             });
           }
         })
@@ -70,7 +79,7 @@ export default (req: Request, res: Response) => {
     });
   } else {
     res.status(400).send({
-      error: 'Missing \'bookid\' and \'libraryid\' params',
+      error: 'Missing \'id\' for the book id, and \'libraryid\' params',
     });
   }
 }
